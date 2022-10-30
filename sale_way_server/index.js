5e4326e69c2da0242d482dcb501c9be09a5e0aec
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-30 23:57:59 +0200

    backend > adding stores route

--- sale_way_server/index.js
+++ sale_way_server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const authMiddleware = require("./middlewares/auth.middleware");
 
 app.use(express.json());
 
-const userRoutes = require("./routes/auth.routes");
-app.use("/", userRoutes);
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
 
 // const userRoutes = require("./routes/users.routes");
 // app.use("/users", userRoutes);
+
+const storeRoutes = require("./routes/store.routes");
+app.use("/stores", authMiddleware, storeRoutes);
 
 app.listen(process.env.PORT, (err) => {
 	if (err) console.log(err);
