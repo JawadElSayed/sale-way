0dac2641d001c748271260c43d791516e8e91ab8
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-31 22:19:28 +0200

    backend > updating manager get all products

--- server/controllers/store.controller.js
+++ server/controllers/store.controller.js
@@ -4,36 +4,12 @@ const prisma = new PrismaClient();
 
 const getAllProducts = async (req, res) => {
 	try {
-		// getting user id from login email
-		const user_id = await prisma.users.findUnique({
-			where: {
-				email: req.email,
-			},
-			select: {
-				id: true,
-			},
-		});
-
-		// getting branches id for the user
-		const branches_id = await prisma.accesses.findMany({
-			where: {
-				user_id: user_id["id"],
-			},
-			select: {
-				branch_id: true,
-			},
-		});
-
-		// put branches id in array
-		const branches_id_array = [];
-		for (let branch of branches_id) {
-			branches_id_array.push(branch["branch_id"]);
-		}
-
 		// getting products
 		const products = await prisma.products.findMany({
 			where: {
-				branch_id: { in: branches_id_array },
+				branches: {
+					accesses: { some: { users: { email: req.email } } },
+				},
 			},
 			include: {
 				images: {
