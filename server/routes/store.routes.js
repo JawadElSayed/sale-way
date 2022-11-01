b3b2fc344d1c09b4cd0a53e4a94b70aabcb77f21
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-01 18:35:03 +0200

    backend > adding edit product route

--- server/routes/store.routes.js
+++ server/routes/store.routes.js
@@ -5,11 +5,13 @@ const {
 	getProduct,
 	productSearch,
 	addProduct,
+	editProduct,
 } = require("../controllers/store.controller");
 
 router.get("/products", getAllProducts);
 router.get("/product/:id", getProduct);
 router.get("/products/search/:search", productSearch);
 router.post("/add_product", addProduct);
+router.post("/edit_product", editProduct);
 
 module.exports = router;
