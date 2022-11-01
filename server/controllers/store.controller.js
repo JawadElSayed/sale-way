b39e0edff22a88b8fb82026e5beffa5a3e62a8e3
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-01 17:18:07 +0200

    backend > saving images in add product

--- server/controllers/store.controller.js
+++ server/controllers/store.controller.js
@@ -1,6 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
 
 const prisma = new PrismaClient();
+
+// timeout function
+function sleep(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
 
 const getAllProducts = async (req, res) => {
 	try {
@@ -131,6 +139,34 @@ const addProduct = async (req, res) => {
 
 		let category_id = 0;
 		if (category) category_id = category["id"];
+
+		// saving images in folder
+		const images_array = [];
+
+		for (let image of body.images) {
+			// spliting base64
+			const splited_image = image.split(";base64,");
+			const image_base64 = splited_image[1];
+			const image_extension = splited_image[0].split("/")[1];
+			// generating unique name acourding it time
+			await sleep(1);
+			const image_path = `./public/images/products/${Date.now()}.${image_extension}`;
+
+			// saving image in folder
+			fs.writeFile(image_path, image_base64, "base64", (err) => {
+				if (err) {
+					return res.status(400).json({
+						message: err.message,
+					});
+				}
+			});
+			images_array.push({ image: image_path });
+		}
+		// setting default case
+		if (images_array.length == 0)
+			images_array.push({
+				image: `./public/images/products/default.png`,
+			});
 	} catch (err) {
 		console.error(err);
 		res.status(400).json({
