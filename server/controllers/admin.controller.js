3313270f9d4264927b064da418640f2bf3d9710d
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-01 22:02:32 +0200

    backend > creating add branch

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -18,7 +18,7 @@ const addStore = async (req, res) => {
 			const image_extension = splited_image[0].split("/")[1];
 
 			// generating unique name acourding it time
-			image_path = `./public/images/products/${Date.now()}.${image_extension}`;
+			image_path = `./public/images/store/${Date.now()}.${image_extension}`;
 
 			// saving image in folder
 			fs.writeFile(image_path, image_base64, "base64", (err) => {
@@ -76,8 +76,79 @@ const getBranchesOfStore = async (req, res) => {
 	}
 };
 
+const addBranch = async (req, res) => {
+	const { ...body } = req.body;
+
+	try {
+		// get category
+		const category = await prisma.categories.findFirst({
+			where: { category: body.category.toLowerCase() },
+		});
+
+		let category_id = 0;
+		if (category) category_id = category["id"];
+
+		// saving image
+		// setting default case
+		let image_path;
+		if (!body.image) {
+			image_path = `./public/images/store/default.jpg`;
+		} else {
+			// spliting base64
+			const splited_image = body.image.split(";base64,");
+			const image_base64 = splited_image[1];
+			const image_extension = splited_image[0].split("/")[1];
+
+			// generating unique name acourding it time
+			image_path = `./public/images/store/${Date.now()}.${image_extension}`;
+
+			// saving image in folder
+			fs.writeFile(image_path, image_base64, "base64", (err) => {
+				if (err) {
+					return res.status(400).json({
+						message: err.message,
+					});
+				}
+			});
+		}
+
+		// creating branch
+		const branch = await prisma.branches.create({
+			data: {
+				name: body.name,
+				about: body.about,
+				phone: body.phone,
+				store_id: body.id,
+				latitude: body.latitude,
+				longitude: body.longitude,
+				image: image_path,
+				store_types: {
+					create: {
+						categories: {
+							connectOrCreate: {
+								where: { id: category_id },
+								create: {
+									category: body.category.toLowerCase(),
+								},
+							},
+						},
+					},
+				},
+			},
+		});
+
+		res.status(200).json({ branch: branch });
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({
+			message: err.message,
+		});
+	}
+};
+
 module.exports = {
 	addStore,
 	getAllBranches,
 	getBranchesOfStore,
+	addBranch,
 };
--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -4,10 +4,12 @@ const {
 	addStore,
 	getAllBranches,
 	getBranchesOfStore,
+	addBranch,
 } = require("../controllers/admin.controller");
 
 router.post("/add_store", addStore);
 router.get("/branches", getAllBranches);
 router.get("/branches/:id", getBranchesOfStore);
+router.post("/add_branch", addBranch);
 
 module.exports = router;
