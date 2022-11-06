e897c181ce596aed66c51fff718c6548952f1de4
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-06 20:38:07 +0200

    frontend > creating useLogin

---
+++ frontend/src/Hooks/useLogin.jsx
@@ -0,0 +1,14 @@
+import { useMutation } from "react-query";
+import { sale_way } from "../utils/axios";
+
+const login = async (data) => {
+	return await sale_way.postAPI(`${sale_way.baseURL}/auth/login`, data);
+};
+
+export const useLogin = () => {
+	return useMutation(login, {
+		onError: (error) => {
+			return error;
+		},
+	});
+};
