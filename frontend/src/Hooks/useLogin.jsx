3ef0747730df5a2010f98a525c09ad11cf5ce25f
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-09 22:04:20 +0200

    frontend > formatting axios

--- frontend/src/Hooks/useLogin.jsx
+++ frontend/src/Hooks/useLogin.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "react-query";
 import { sale_way } from "../utils/axios";
 
 const login = async (data) => {
-	return await sale_way.postAPI(`${sale_way.baseURL}/auth/login`, data);
+	return await sale_way.postAPI(`/auth/login`, data);
 };
 
 export const useLogin = () => {
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,16 +2,15 @@ import axios from "axios";
 
 export const sale_way = {};
 
-sale_way.baseURL = "http://127.0.0.1:3000";
+const token = localStorage.getItem("token");
+
+axios.defaults.baseURL = "http://127.0.0.1:3000";
+axios.defaults.headers = { Authorization: "token " + token };
 
 sale_way.getAPI = async (api_url) => {
 	return await axios(api_url);
 };
 
-sale_way.postAPI = async (api_url, api_data, api_token = null) => {
-	return await axios.post(api_url, api_data, {
-		headers: {
-			Authorization: "token " + api_token,
-		},
-	});
+sale_way.postAPI = async (api_url, api_data) => {
+	return await axios.post(api_url, api_data);
 };
