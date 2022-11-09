eff1096398c18a751c9ab351429c4f77416e11ec
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-10 00:44:56 +0200

    frontend > creating store routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import Form from "./components/Form";
-import Users from "./Pages/users.page";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import Products from "./Pages/products.page";
+import LandingPage from "./Pages/landing.page";
+import AddProduct from "./Pages/addProduct.page";
 
 const queryClient = new QueryClient();
+
+const storePages = ["Products", "Analytics", "Profile"];
 
 function App() {
 	return (
@@ -12,8 +16,33 @@ function App() {
 			<Router>
 				<div className="site-font">
 					<Routes>
-						<Route exact path="/" element={<Form />}></Route>
-						<Route exact path="/users"></Route>
+						<Route exact path="/" element={<LandingPage />}></Route>
+
+						<Route
+							path="/store"
+							element={<Layout buttons={storePages} />}
+						>
+							<Route
+								exact
+								path="products"
+								element={<Products />}
+							/>
+							<Route
+								exact
+								path="analytics"
+								element={<Products />}
+							/>
+							<Route
+								exact
+								path="profile"
+								element={<Products />}
+							/>
+							<Route
+								exact
+								path="add-product"
+								element={<AddProduct />}
+							/>
+						</Route>
 					</Routes>
 				</div>
 			</Router>
