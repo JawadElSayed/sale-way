ba9dedba95bb5ada799b9d6f1211c3dd9050bf9d
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-16 21:08:45 +0200

    mobile > returning response http

--- mobile/lib/helpers/http/http.dart
+++ mobile/lib/helpers/http/http.dart
@@ -3,33 +3,31 @@ import 'package:http/http.dart' as http;
 const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imphd2FkMkB0ZXN0LmNvbSIsIm5hbWUiOiJqYXdhZCIsInVzZXJUeXBlIjoyLCJpYXQiOjE2Njg2MTYwMjksImV4cCI6MTY2ODYxOTYyOX0.NjI0k85QdpnDPcTIZzFU7FiDUCdleThZR2KCwRhV_70";
 
-const baseURL = "localhost:3000";
+Map<String, String> headers = {
+  'Content-Type': 'application/json',
+  'token': 'Bearer $token'
+};
+
+const baseURL = "192.168.44.166:3000";
 Future get(link) async {
   final url = Uri.http(baseURL, link);
 
-  http.get(url, headers: {"token": "Bearer $token"});
+  return await http.get(url, headers: {"token": "Bearer $token"});
 }
 
 Future post(link, body) async {
   final url = Uri.http(baseURL, link);
-
-  http.post(url,
-      body: body,
-      headers: {"Content-Type": "application/json", "token": "Bearer $token"});
+  return await http.post(url, body: body, headers: headers);
 }
 
 Future put(link, body) async {
   final url = Uri.http(baseURL, link);
 
-  http.put(url,
-      body: body,
-      headers: {"Content-Type": "application/json", "token": "Bearer $token"});
+  return await http.put(url, body: body, headers: headers);
 }
 
 Future delete(link, body) async {
   final url = Uri.http(baseURL, link);
 
-  http.delete(url,
-      body: body,
-      headers: {"Content-Type": "application/json", "token": "Bearer $token"});
+  return await http.delete(url, body: body, headers: headers);
 }
