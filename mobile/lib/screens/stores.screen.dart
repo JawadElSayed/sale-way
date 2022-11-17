2e37a2bbcd9d4d2865e492074668fc95f1883c38
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-17 23:52:10 +0200

    mobile > creating basic store page

--- mobile/lib/screens/stores.screen.dart
+++ mobile/lib/screens/stores.screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/widgets/store_card.dart';
+import 'package:mobile/widgets/top_bar.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/branches.provider.dart';
 
 class StoresScreen extends StatelessWidget {
   const StoresScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Stores Screen"),
+    List branches = Provider.of<Branches>(context).branchesGetter;
+
+    return Container(
+      child: Column(
+        children: [
+          Container(child: TopBar()),
+          Container(
+            height: 500,
+            child: ListView(
+              children: branches.map((branch) {
+                return StoreCard(branch: branch);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
