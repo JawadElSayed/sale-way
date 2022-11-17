27c60e41792416fe8b1bd49a2cc6d756b5c7b958
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-17 23:52:29 +0200

    creating > basic product page

--- mobile/lib/screens/products.screen.dart
+++ mobile/lib/screens/products.screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import '../widgets/product_card.dart';
+import '../providers/products.provider.dart';
+import 'package:provider/provider.dart';
+import '../widgets/top_bar.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Products Screen"),
+    List products = Provider.of<Products>(context).productGetter;
+    return Container(
+      child: Column(
+        children: [
+          Container(child: TopBar()),
+          Container(
+            height: 500,
+            child: ListView(
+              children: products.map((product) {
+                return ProductCard(product: product);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
