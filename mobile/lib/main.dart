b4b59cb6e00c61c0309d6c29df9363ebebb413e7
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-17 23:49:22 +0200

    mobile > adding providers to main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import './providers/products.provider.dart';
+import './providers/branches.provider.dart';
 import './screens/tabs.screen.dart';
 import './screens/map.screen.dart';
 import './screens/notification.screen.dart';
@@ -12,8 +14,7 @@ import './screens/stores.screen.dart';
 import './screens/login.screen.dart';
 import './screens/store.screen.dart';
 import 'helpers/notificationservice/local_notification_service.dart';
-import "./helpers/notificationservice/notification.dart" as notification;
-import "./helpers/locationService/location.dart" as location;
+import 'package:provider/provider.dart';
 
 Future<void> backgroundHandler(RemoteMessage message) async {
   LocalNotificationService.createanddisplaynotification(message);
@@ -25,35 +26,44 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   LocalNotificationService.initialize();
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    location.livePosition();
-    notification.getToken();
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Branches(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Products(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Color.fromARGB(255, 112, 133, 238),
+          accentColor: Color.fromARGB(255, 155, 170, 243),
+        ),
+        initialRoute: "/",
+        routes: {
+          "/": (ctx) => const TabScreen(),
+          "/signup": (ctx) => const SignUpScreen(),
+          "/login": (ctx) => const LoginScreen(),
+          "/stores": (ctx) => StoresScreen(),
+          "/store": (ctx) => const StoreScreen(),
+          "/products": (ctx) => const ProductsScreen(),
+          "/product": (ctx) => const ProductScreen(),
+          "/map": (ctx) => const MapScreen(),
+          "/notification": (ctx) => const NotificationScreen(),
+          "/profile": (ctx) => const ProfileScreen(),
+        },
       ),
-      initialRoute: "/",
-      routes: {
-        "/": (ctx) => TabScreen(),
-        "/signup": (ctx) => SignUpScreen(),
-        "/login": (ctx) => LoginScreen(),
-        "/stores": (ctx) => StoresScreen(),
-        "/store": (ctx) => StoreScreen(),
-        "/products": (ctx) => ProductsScreen(),
-        "/product": (ctx) => ProductScreen(),
-        "/map": (ctx) => MapScreen(),
-        "/notification": (ctx) => NotificationScreen(),
-        "/profile": (ctx) => ProfileScreen(),
-      },
     );
   }
 }
