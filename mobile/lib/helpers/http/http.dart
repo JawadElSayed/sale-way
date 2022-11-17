8fad30f261aee76d082d5da4216412a40a8004b3
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-17 23:44:29 +0200

    mobile > correcting authorization

--- mobile/lib/helpers/http/http.dart
+++ mobile/lib/helpers/http/http.dart
@@ -1,18 +1,18 @@
 import 'package:http/http.dart' as http;
 
 const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imphd2FkMkB0ZXN0LmNvbSIsIm5hbWUiOiJqYXdhZCIsInVzZXJUeXBlIjoyLCJpYXQiOjE2Njg2MTYwMjksImV4cCI6MTY2ODYxOTYyOX0.NjI0k85QdpnDPcTIZzFU7FiDUCdleThZR2KCwRhV_70";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imphd2FkMkB0ZXN0LmNvbSIsIm5hbWUiOiJqYXdhZCIsInVzZXJUeXBlIjoyLCJpYXQiOjE2Njg3MTUwNDAsImV4cCI6MTY2ODcxODY0MH0.NQRfOPnGHdJlRPCKPbqlGPA-9hM32ZHa1yfK2ZK-Ndg";
 
 Map<String, String> headers = {
   'Content-Type': 'application/json',
-  'token': 'Bearer $token'
+  'authorization': 'Bearer $token'
 };
 
 const baseURL = "192.168.44.166:3000";
 Future get(link) async {
   final url = Uri.http(baseURL, link);
 
-  return await http.get(url, headers: {"token": "Bearer $token"});
+  return await http.get(url, headers: {"authorization": "Bearer $token"});
 }
 
 Future post(link, body) async {
