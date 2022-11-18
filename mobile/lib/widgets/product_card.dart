7bc6073a433c82539125baa9d2e99144c7be767e
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-18 03:36:47 +0200

    mobile > adding on tap product

--- mobile/lib/widgets/product_card.dart
+++ mobile/lib/widgets/product_card.dart
@@ -5,38 +5,46 @@ class ProductCard extends StatelessWidget {
   final Product product;
   const ProductCard({required this.product, super.key});
 
+  void selectProduct(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed("/product", arguments: product);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(20, 10, 40, 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                    padding: EdgeInsets.only(right: 20), child: Text("image")),
-                Column(
-                  children: [
-                    Container(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Text(product.name)),
-                    Text("category")
-                  ],
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text("discount")),
-                Text("distance")
-              ],
-            )
-          ],
+    return InkWell(
+      onTap: () => selectProduct(context),
+      child: Card(
+        elevation: 0,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 10, 40, 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text("image")),
+                  Column(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(product.name)),
+                      Text("category")
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text("discount")),
+                  Text("distance")
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
