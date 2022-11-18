c4f14afabd5fce8864ed3f24078719f6266884aa
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-18 14:34:19 +0200

    backend > creating notification route

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
-const {} = require("../controllers/notification.controller");
+const { sendNotification } = require("../controllers/notification.controller");
 const router = Router();
+
+router.get("/", sendNotification);
 
 module.exports = router;
