0b4bcbe7ed9b9bda078fdc83ff531337244f1801
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-18 16:44:36 +0200

    backend > save notification

--- server/controllers/notification.controller.js
+++ server/controllers/notification.controller.js
@@ -31,11 +31,22 @@ const sendNotification = async (req, res) => {
 			},
 		};
 
-		fcm.send(message, (err, response) => {
+		fcm.send(message, async (err, response) => {
 			if (err) {
 				return res.status(400).json({ message: err });
 			}
 			res.status(200).json(response);
+			const firebase_id = response.split("messages/")[1];
+			
+			const save = await prisma.notifications.create({
+				data: {
+					firebase_id: firebase_id,
+					title: req.body.title,
+					message: req.body.body,
+					users: { connect: { email: req.email } },
+					branches: { connect: { id: req.body.branch_id } },
+				},
+			});
 		});
 	} catch (err) {
 		res.status(400).json({ message: err.message });
