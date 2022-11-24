abfdac3f50b177984ab8665e7027d8b4e5f2ef3d
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 00:02:53 +0200

    backend > adding analytics routes

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -3,6 +3,8 @@ const {
 	sendNotification,
 	getUserNotifications,
 	clickNotification,
+	getAllNotificationAnalytics,
+	getBranchAnalytics,
 	deleteNotification,
 } = require("../controllers/notification.controller");
 const router = Router();
@@ -10,6 +12,8 @@ const router = Router();
 router.post("/", sendNotification);
 router.get("/user", getUserNotifications);
 router.post("/click", clickNotification);
+router.get("/", getAllNotificationAnalytics);
+router.get("/:id", getBranchAnalytics);
 router.delete("/:id", deleteNotification);
 
 module.exports = router;
