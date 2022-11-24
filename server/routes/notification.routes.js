858f6dd4bf7702a86ee5817fb08f1e7f484789d5
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 00:01:08 +0200

    backend > adding click notification route

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
-const { sendNotification,getUserNotifications } = require("../controllers/notification.controller");
+const {
+	sendNotification,
+	getUserNotifications,
+	clickNotification,
+} = require("../controllers/notification.controller");
 const router = Router();
 
 router.post("/", sendNotification);
-router.post("/user", getUserNotifications);
+router.get("/user", getUserNotifications);
+router.post("/click", clickNotification);
 
 module.exports = router;
