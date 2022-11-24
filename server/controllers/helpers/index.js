65a6118f7c60be46a2afac70bf7d0994c11fd647
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-24 23:54:56 +0200

    backend > adding helper delet branches

--- server/controllers/helpers/index.js
+++ server/controllers/helpers/index.js
@@ -3,25 +3,30 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const deleteBranch = async (req) => {
-	// delete connection of accesses
-	await prisma.accesses.deleteMany({
-		where: { branch_id: parseInt(req.params.id) },
-	});
+	try {
+		// delete connection of accesses
+		await prisma.accesses.deleteMany({
+			where: { branch_id: parseInt(req.params.id) },
+		});
 
-	// delete connection of store types
-	await prisma.store_types.deleteMany({
-		where: { branch_id: parseInt(req.params.id) },
-	});
+		// delete connection of store types
+		await prisma.store_types.deleteMany({
+			where: { branch_id: parseInt(req.params.id) },
+		});
 
-	// delete connection of products
-	await prisma.products.deleteMany({
-		where: { branches: { every: { id: parseInt(req.params.id) } } },
-	});
+		// delete connection of products
+		await prisma.products.deleteMany({
+			where: { branches: { every: { id: parseInt(req.params.id) } } },
+		});
 
-	// delete branch
-	const branch = await prisma.branches.delete({
-		where: { id: parseInt(req.params.id) },
-	});
+		// delete branch
+		const branch = await prisma.branches.delete({
+			where: { id: parseInt(req.params.id) },
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: err.message });
+	}
 };
 
 module.exports = { deleteBranch };
