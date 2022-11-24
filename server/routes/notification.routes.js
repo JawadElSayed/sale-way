deee09636e923a8e2102c4c693750297dabb1b32
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 00:03:42 +0200

    backend > adding best analytics routes

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -6,12 +6,16 @@ const {
 	getAllNotificationAnalytics,
 	getBranchAnalytics,
 	deleteNotification,
+	getBestUser,
+	getBestBranch,
 } = require("../controllers/notification.controller");
 const router = Router();
 
 router.post("/", sendNotification);
 router.get("/user", getUserNotifications);
 router.post("/click", clickNotification);
+router.get("/best-user", getBestUser);
+router.get("/best-branch", getBestBranch);
 router.get("/", getAllNotificationAnalytics);
 router.get("/:id", getBranchAnalytics);
 router.delete("/:id", deleteNotification);
