0643817197dd42fa53e4fd94adc0568d9572b2a9
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 00:02:04 +0200

    backend> adding delete notification route

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -3,11 +3,13 @@ const {
 	sendNotification,
 	getUserNotifications,
 	clickNotification,
+	deleteNotification,
 } = require("../controllers/notification.controller");
 const router = Router();
 
 router.post("/", sendNotification);
 router.get("/user", getUserNotifications);
 router.post("/click", clickNotification);
+router.delete("/:id", deleteNotification);
 
 module.exports = router;
