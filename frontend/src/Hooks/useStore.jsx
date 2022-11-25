b52912ab2d63149d5f6a2bb7cd1bcb3d896a9f62
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:08:20 +0200

    frontend > creating add store

--- frontend/src/Hooks/useStore.jsx
+++ frontend/src/Hooks/useStore.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { sale_way } from "../utils/axios";
 
 export const ALL_STORES_KEY = ["ALL_STORE_KEY"];
@@ -7,6 +7,14 @@ const getAllStores = async () => {
 	return await sale_way.getAPI(`/store`);
 };
 
+const AddStore = async (data) => {
+	return await sale_way.postAPI("/store", data);
+};
+
 export const useAllStores = () => {
 	return useQuery(ALL_STORES_KEY, getAllStores);
+};
+
+export const useAddStore = () => {
+	return useMutation(AddStore);
 };
