9039b5e0f82a7a5dfa5b68090b7b24cec09221e5
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 23:17:51 +0200

    mobile > adding to bar notification screen

--- mobile/lib/screens/notification.screen.dart
+++ mobile/lib/screens/notification.screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
 
   @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Notification Screen"),
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.20,
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Notifications",
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () {},
+                    ),
+                  ],
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
