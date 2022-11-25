5dab200ea0fa48e10441a4b2e275863cfb628917
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:34:32 +0200

    frontend > creating line graph

---
+++ frontend/src/components/LineGraph.jsx
@@ -0,0 +1,29 @@
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
+);
+
+const LineGraph = ({ labels, graphData }) => {
+	return;
+};
+
+export default LineGraph;
