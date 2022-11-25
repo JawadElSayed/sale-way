7be9266bac059ca5fe5effbb336b39eea22cd001
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 22:41:48 +0200

    mobile > updating style circular image

--- mobile/lib/widgets/circle_image.dart
+++ mobile/lib/widgets/circle_image.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 
 class CircleImage extends StatelessWidget {
   final url;
-  const CircleImage({super.key, this.url});
+  int radius = 30;
+  CircleImage({super.key, this.url, required this.radius});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60,
-      height: 60,
+      width: radius * 2,
+      height: radius * 2,
       margin: const EdgeInsets.only(right: 15),
       decoration: BoxDecoration(
         color: const Color(0xff7c94b6),
         image: DecorationImage(
-          image: NetworkImage(url),
+          image: NetworkImage("http://$url"),
           fit: BoxFit.cover,
         ),
-        borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+        borderRadius: const BorderRadius.all(Radius.circular(100.0)),
         border: Border.all(
           color: Theme.of(context).accentColor,
           width: 2.0,
