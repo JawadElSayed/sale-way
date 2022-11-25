eb1f445efb17f6ec4f71389839a064efd4c90db0
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 15:18:13 +0200

    frontend > adding logic add user

--- frontend/src/Pages/addUser.page.jsx
+++ frontend/src/Pages/addUser.page.jsx
@@ -1,15 +1,53 @@
 import { useState, useEffect } from "react";
+import { useAddUser } from "../Hooks/useUser";
+import { useNavigate } from "react-router-dom";
 
+import Button from "../components/Button";
 import TextInput from "../components/TextInput";
 import Header from "../layouts/header";
 
 const AddUser = () => {
+	const navigate = useNavigate();
 
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [gender, setGender] = useState("");
 	const [DOB, setDOB] = useState("");
 	const [error, setError] = useState("");
+
+	const { mutate, isSuccess, isError, data } = useAddUser();
+
+	const saveUser = () => {
+		setError("");
+		if (validation(name, email, gender, DOB))
+			return setError(validation(name, email, gender, DOB));
+		const data = {
+			name: name,
+			email: email,
+			gender: gender,
+			DOB: DOB,
+			user_type: "store manager",
+		};
+
+		mutate(data);
+		if (isError) alert("Something went wrong");
+	};
+
+	useEffect(() => {
+		if (isSuccess) {
+			alert(`password: ${data.data.password}`);
+			navigate("/admin/users");
+		}
+	}, [isSuccess, navigate]);
+
+	const validation = (name, email, gender, DOB) => {
+		if (!name) return "Name fields is required";
+		if (!email) return "Email fields is required";
+		if (!gender) return "Gender fields is required";
+		if (!DOB) return "Date of birth fields is required";
+
+		return "";
+	};
 
 	return (
 		<div className="flex flex-col h-screen">
@@ -44,6 +82,15 @@ const AddUser = () => {
 				{error && (
 					<p className="text-red-700 pt-2 text-left">{error}</p>
 				)}
+				<div className="text-center">
+					<Button
+						className="mt-12 w-32"
+						rounded="xl"
+						onClick={saveUser}
+					>
+						Save
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
