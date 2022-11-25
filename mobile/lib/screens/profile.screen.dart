94da6504f0803b0c8adc3b0a08ce3b4b4620f81c
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-26 00:12:16 +0200

    mobile > adding profile image

--- mobile/lib/screens/profile.screen.dart
+++ mobile/lib/screens/profile.screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/providers/user.provider.dart';
+import 'package:mobile/widgets/circle_image.dart';
+import 'package:provider/provider.dart';
+import '../helpers/config/config.dart';
+import '../providers/auth.provider.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final nameCntroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Screen"),
+    User user = Provider.of<Auth>(context).userGetter;
+    // _selectedGender = user.gender;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+      height: MediaQuery.of(context).size.height * 0.82,
+      child: SingleChildScrollView(
+        child: Column(children: [
+          CircleImage(
+              url: '${Config.staticUrl}/static/images/profile/default.jpeg',
+              radius: 60),
+        ]),
+      ),
     );
   }
 }
