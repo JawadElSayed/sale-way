267816c852c9f4e0712c360cc2805961222e4622
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:14:03 +0200

    frontend > creating add/edit branch

--- frontend/src/Hooks/useBranches.jsx
+++ frontend/src/Hooks/useBranches.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { sale_way } from "../utils/axios";
 
 export const ALL_STORES_KEY = ["ALL_BRANCHES_KEY"];
@@ -8,12 +8,24 @@ const getAllBranches = async () => {
 	return await sale_way.getAPI(`/branch`);
 };
 
+const AddBranch = async (data) => {
+	return await sale_way.postAPI("/branch", data);
+};
+
 const getBranchByID = async (id) => {
 	return await sale_way.getAPI(`/branch/${id}`);
 };
 
+const editBranch = async (data) => {
+	return await sale_way.putAPI(`/branch`);
+};
+
 export const useAllBranches = () => {
 	return useQuery(ALL_STORES_KEY, getAllBranches);
+};
+
+export const useAddBranch = () => {
+	return useMutation(AddBranch);
 };
 
 export const useBranchByID = (id) => {
@@ -21,4 +33,8 @@ export const useBranchByID = (id) => {
 		queryKey: buildBranchById(id),
 		queryFn: () => getBranchByID(id),
 	});
+};
+
+export const useEditBranch = () => {
+	return useMutation(editBranch);
 };
