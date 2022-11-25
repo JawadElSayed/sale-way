bff3bb37d9577ca74d019fc0c7becc43ccafb055
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 16:38:46 +0200

    frontend > creating labels of graph in store page

--- frontend/src/Pages/AdminStore.page.jsx
+++ frontend/src/Pages/AdminStore.page.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useBranchByID } from "../Hooks/useBranches";
 import Header from "../layouts/header";
+import DropList from "../components/DropList";
+import LineGraph from "../components/LineGraph";
 import Table from "../components/Table";
 
 const AdminStore = () => {
@@ -15,6 +18,8 @@ const AdminStore = () => {
 		"Discount",
 		"Action",
 	];
+	const filterList = ["Last Week", "Last Month", "Last Year"];
+	const [filter, setFilter] = useState("Last Week");
 
 	const {
 		isLoading: branchLoading,
@@ -44,7 +49,45 @@ const AdminStore = () => {
 		});
 	}
 
-	return (
+	let date = new Date();
+
+	const weekList = [
+		`${date.getDate() - 6}`,
+		`${date.getDate() - 5}`,
+		`${date.getDate() - 4}`,
+		`${date.getDate() - 3}`,
+		`${date.getDate() - 2}`,
+		`${date.getDate() - 1}`,
+		`${date.getDate()}`,
+	];
+	let monthList = [];
+	for (let i = 29; i >= 0; i--) {
+		if (date.getDate() - i === 0) monthList.push(`30`);
+		else if (date.getDate() - i < 0)
+			monthList.push(`${date.getDate() - i + 30}`);
+		else monthList.push(`${date.getDate() - i}`);
+	}
+
+	let yearList = [];
+	for (let i = 11; i >= 0; i--) {
+		if (date.getMonth() - i === 0) yearList.push(`12`);
+		else if (date.getMonth() - i < 0)
+			yearList.push(`${date.getMonth() - i + 12}`);
+		else yearList.push(`${date.getMonth() - i}`);
+	}
+
+	// console.log(labels);
+	let graphDataList = [];
+
+	useEffect(() => {
+		if (filter === "Last Week") setLabels(weekList);
+		if (filter === "Last Month") setLabels(monthList);
+		if (filter === "Last Year") setLabels(yearList);
+
+		setgraphData(graphDataList);
+	}, [filter]);
+
+	const [labels, setLabels] = useState(weekList);
 		<div className="flex flex-col h-screen">
 			<Header
 				image={!branchLoading && branch?.image}
@@ -54,6 +97,18 @@ const AdminStore = () => {
 			></Header>
 			<div className="overflow-y-scroll hide-scroll-bar pt-2">
 				{branchLoading && <h2>Loading...</h2>}
+				<div>
+					<DropList
+						value={filter}
+						options={filterList}
+						onChange={(e) => setFilter(e.target.value)}
+						className="text-right"
+					/>
+				</div>
+
+				<div className="w-full my-12">
+					<LineGraph graphData={graphData} labels={labels} />
+				</div>
 				<div className="pt-8 pb-12">
 					<Table
 						titles={tableTitles}
