6c62ceaa0d49af9f6631cff0fe95368e68d1c70a
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 23:31:41 +0200

    mobile > filling data in product screen

--- mobile/lib/screens/product.screen.dart
+++ mobile/lib/screens/product.screen.dart
@@ -1,102 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/widgets/circle_image.dart';
-import '../providers/product.provider.dart';
+import '../helpers/config/config.dart';
+import '../providers/products.provider.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
   ProductScreen({super.key});
 
   @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  bool isLoading = true;
+  var product;
+
+  @override
   Widget build(BuildContext context) {
-    final product = ModalRoute.of(context)?.settings.arguments as Product;
+    var product_id = ModalRoute.of(context)?.settings.arguments;
+
+    if (product == null) {
+      getProduct(product_id).then((p) {
+        product = p;
+        isLoading = false;
+        setState(() {});
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(backgroundColor: Theme.of(context).primaryColor),
-      body: Container(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            height: 330,
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: DecorationImage(
-                image: NetworkImage(
-                    'http://192.168.0.103:3000/static/images/store/default.jpg'),
-                fit: BoxFit.cover,
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Container(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 330,
+                        decoration: BoxDecoration(color: Colors.grey[200]),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'http://${Config.staticUrl}${product.images[0]["image"]}'),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding:
+                            const EdgeInsets.only(left: 30, top: 30, right: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Text(
+                                  product.name,
+                                  style: Theme.of(context).textTheme.headline1,
+                                )),
+                            Padding(
+                                padding: const EdgeInsets.only(bottom: 30),
+                                child: Text(product.description)),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: Row(
+                                children: [
+                                  CircleImage(
+                                      url:
+                                          '${Config.staticUrl}${product.branches[0]["image"]}',
+                                      radius: 30),
+                                  Text(
+                                    product.branches[0]["name"],
+                                    style:
+                                        Theme.of(context).textTheme.headline3,
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: Text("Discount: ${product.discount}%"),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: Text("Distance: 1Km"),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 25),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            Theme.of(context).accentColor,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.of(context).pushNamed("/map");
+                                      },
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 13),
+                                        width: 100,
+                                        alignment: Alignment.center,
+                                        child: Text("Location",
+                                            style:
+                                                TextStyle(color: Colors.white)),
+                                      )),
+                                  ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: const Color.fromRGBO(
+                                            36, 180, 59, 1),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                      ),
+                                      onPressed: () {},
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 13),
+                                        width: 100,
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            children: const [
+                                              // Icon(Icons.whatsapp,
+                                              //     color: Colors.white),
+                                              Text("WhatsApp",
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                  )),
+                                            ]),
+                                      ))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ]),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(product.name)),
-                Padding(
-                    padding: const EdgeInsets.only(bottom: 30),
-                    child: Text(product.description)),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Row(
-                    children: [
-                      const CircleImage(
-                          url:
-                              'http://192.168.0.103:3000/static/images/store/default.jpg'),
-                      Text(product.name),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Text("Discount: ${product.discount}%"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Text("Distance: Km"),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context).accentColor,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        onPressed: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 13),
-                          width: 100,
-                          alignment: Alignment.center,
-                          child: Text("Location",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color.fromRGBO(36, 180, 59, 1),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        onPressed: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 9),
-                          width: 100,
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: const [
-                                Icon(Icons.whatsapp, color: Colors.white),
-                                Text("WhatsApp",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    )),
-                              ]),
-                        ))
-                  ],
-                )
-              ],
-            ),
-          )
-        ]),
-      ),
     );
   }
 }
