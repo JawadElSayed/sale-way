768d518a07c84ae91135e5d2de6060787cdf18e7
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 17:03:33 +0200

    frontend > adding analyticas cards

--- frontend/src/Pages/adminAnalytics.page.jsx
+++ frontend/src/Pages/adminAnalytics.page.jsx
@@ -1,10 +1,96 @@
+import StatisticsCard from "../components/statisticCard";
 import Header from "../layouts/header";
 
+import {
+	useNumberClicks,
+	useBestUsers,
+	useBestBranches,
+} from "../Hooks/useNotifications";
 
 const AdminAnalytics = () => {
+	const { isLoading: clicksLoading, data: clicksData } = useNumberClicks();
+	const { isLoading: userLoading, data: usersData } = useBestUsers();
+	const { isLoading: branchLoading, data: branchData } = useBestBranches();
 	return (
 		<>
 			<Header title="Analytics"></Header>
+			{clicksLoading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className="pt-8">
+					<div className="flex flex-col gap-4">
+						<div className="flex gap-8">
+							<StatisticsCard
+								color="red"
+								title="Number of clicks"
+								value={clicksData.data.lastWeek}
+							/>
+							<StatisticsCard
+								color="green"
+								title="Number of clicks"
+								value={clicksData.data.lastMonth}
+							/>
+							<StatisticsCard
+								color="blue"
+								title="Number of clicks"
+								value={clicksData.data.lastYear}
+							/>
+						</div>
+						<div className="flex gap-8">
+							<StatisticsCard
+								color="red"
+								title="Best Store"
+								name={branchData?.data.nameLastWeek.name}
+								value={
+									branchData?.data.lastWeek[0]._count.clicked
+								}
+							/>
+							<StatisticsCard
+								color="green"
+								title="Best Store"
+								name={branchData?.data.nameLastMonth.name}
+								value={
+									branchData?.data.lastMonth[0]._count.clicked
+								}
+							/>
+							<StatisticsCard
+								color="blue"
+								title="Best Store"
+								name={branchData?.data.nameLastYear.name}
+								value={
+									branchData?.data.lastYear[0]._count.clicked
+								}
+							/>
+						</div>
+						<div className="flex gap-8">
+							<StatisticsCard
+								color="red"
+								title="Best user"
+								name={usersData?.data.nameLastWeek.name}
+								value={
+									usersData?.data.lastWeek[0]._count.clicked
+								}
+							/>
+							<StatisticsCard
+								color="green"
+								title="Best user"
+								name={usersData?.data.nameLastMonth.name}
+								value={
+									usersData?.data.lastMonth[0]._count.clicked
+								}
+							/>
+							<StatisticsCard
+								color="blue"
+								title="Best user"
+								name={usersData?.data.nameLastYear.name}
+								value={
+									usersData?.data.lastYear[0]._count.clicked
+								}
+							/>
+						</div>
+					</div>
+				</div>
+			)}
 		</>
 	);
 };
