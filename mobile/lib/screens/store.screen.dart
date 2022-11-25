ef5fab9d2501eb90a76cc5e82ddecd10fac6f961
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 23:53:10 +0200

    mobile > adding top bar in store screen

--- mobile/lib/screens/store.screen.dart
+++ mobile/lib/screens/store.screen.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/widgets/circle_image.dart';
 
-class StoreScreen extends StatelessWidget {
-  const StoreScreen({super.key});
+import '../helpers/config/config.dart';
+import '../providers/branches.provider.dart';
+import '../widgets/product_card.dart';
+
+class StoreScreen extends StatefulWidget {
+  StoreScreen({super.key});
+
+  @override
+  State<StoreScreen> createState() => _StoreScreenState();
+}
+
+class _StoreScreenState extends State<StoreScreen> {
+  bool _isLoading = true;
+
+  var branch;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Store Screen"),
+    final branch_id = ModalRoute.of(context)?.settings.arguments;
+
+    if (branch == null) {
+      getBranch(branch_id).then((b) {
+        print(b);
+        branch = b;
+        _isLoading = false;
+        setState(() {});
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 1,
+        toolbarHeight: 150,
+        centerTitle: true,
+        title: _isLoading
+            ? null
+            : CircleImage(
+                url: '${Config.staticUrl}${branch.image}',
+                radius: (AppBar().preferredSize.height).toInt()),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Container()
     );
   }
 }
