09c927726c17a3efde066a1e87189ee259f89b3f
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:58:12 +0200

    frontend > fixing products table

--- frontend/src/Pages/products.page.jsx
+++ frontend/src/Pages/products.page.jsx
@@ -2,6 +2,7 @@ import Button from "../components/Button";
 import { useAllProducts } from "../Hooks/useProducts";
 import Header from "../layouts/header";
 import { useNavigate } from "react-router-dom";
+import Table from "../components/Table";
 
 const Products = () => {
 	const titles = [
@@ -14,55 +15,50 @@ const Products = () => {
 
 	const navigate = useNavigate();
 
+	const tableTitles = [
+		"Image",
+		"Product Name",
+		"Category",
+		"Discount",
+		"Actions",
+	];
+
 	const secondRowButtons = [
 		["Add Product", () => navigate("/store/add-product")],
 	];
 
 	const { isLoading, data, isError, error } = useAllProducts();
-
-	if (!isLoading) console.log(data);
+	let tableData = [];
+	if (!isLoading) {
+		data.data.products.map((product) => {
+			const newData = [
+				product.id,
+				product.images?.[0]?.image,
+				product.name,
+				product.product_categories?.[0]?.categories.category,
+				[product.discount],
+				product,
+			];
+			return tableData.push(newData);
+		});
+	}
 
 	return (
 		<div className="flex flex-col h-screen">
 			<Header
 				title="Products"
 				secondRowButtons={secondRowButtons}
-				titles={titles}
 			></Header>
 
-			<div className="overflow-y-scroll hide-scroll-bar pt-2">
+			<div className="overflow-y-scroll hide-scroll-bar pt-8">
 				{isLoading && <h2>Loading...</h2>}
-				{data?.data.products.map((product) => {
-					return (
-						<div
-							key={product.id}
-							className="flex items-center pb-4 gap-6"
-						>
-							<img
-								src={`../../../server/public/images/products/default.png`}
-								alt="product"
-								className="w-20"
-							/>
-							<h3>{product.name}</h3>
-							<h3>{product.name}</h3>
-							<h3>{product.discount}</h3>
-							<div>
-								<Button
-									style={{ width: "8.75vw", minWidth: "fit" }}
-								>
-									Edit
-								</Button>
-								<Button
-									className="ml-2"
-									backgroundColor="red"
-									style={{ width: "8.75vw", minWidth: "fit" }}
-								>
-									Delete
-								</Button>
-							</div>
-						</div>
-					);
-				})}
+				<Table
+					titles={tableTitles}
+					data={tableData}
+					imageClick={imageClick}
+					edit={1}
+					deleteClick={deleteProduct}
+				/>
 			</div>
 		</div>
 	);
