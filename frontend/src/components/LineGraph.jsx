0a831635fa9b19f576f707ac3360b5b3b214b4f7
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:39:08 +0200

    frontend > styling line graph

--- frontend/src/components/LineGraph.jsx
+++ frontend/src/components/LineGraph.jsx
@@ -23,7 +23,23 @@ ChartJS.register(
 );
 
 const LineGraph = ({ labels, graphData }) => {
-	return;
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "Clicks",
+				data: graphData,
+				tension: 0.4,
+				backgroundColor: ["rgba(155, 169, 243,0.2)"],
+				borderColor: ["rgba(155, 169, 243,0.4)"],
+				pointBackgroundColor: ["rgb(155, 169, 243)"],
+				pointBorderColor: ["rgb(155, 169, 243)"],
+				fill: true,
+			},
+		],
+	};
+
+	return <Line data={data} />;
 };
 
 export default LineGraph;
