82a137c5399cce8e4ff9ce9788c76713a94a088f
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 15:15:42 +0200

    frontend > adding header add user

---
+++ frontend/src/Pages/addUser.page.jsx
@@ -0,0 +1,11 @@
+import Header from "../layouts/header";
+
+const AddUser = () => {
+	return (
+		<div className="flex flex-col h-screen">
+			<Header title="Add User"></Header>
+		</div>
+	);
+};
+
+export default AddUser;
