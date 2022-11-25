c50ccea29d0333e64b8aba850877e3425c19b077
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 22:51:02 +0200

    mobile > adding droplist to top bar

--- mobile/lib/widgets/top_bar.dart
+++ mobile/lib/widgets/top_bar.dart
@@ -8,16 +8,26 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 160,
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
       decoration: BoxDecoration(color: Theme.of(context).primaryColor),
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
         child: Column(children: [
           SearchBar(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              DropDown(),
-              DropDown(),
+              DropDown(list: [
+                "category",
+                "clothes",
+                "phones",
+                "laptops",
+                "shoes",
+                "perfuims",
+                "makeups"
+              ]),
+              DropDown(list: ["distance", "1km", "2km", "3km", "5km", "10km"]),
             ],
           ),
         ]),
