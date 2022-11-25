eb7571eced015eabf1293c0193df7df145391c9a
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 22:47:15 +0200

    mobile > adding data to product card

--- mobile/lib/widgets/product_card.dart
+++ mobile/lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../helpers/config/config.dart';
 import '../providers/product.provider.dart';
 import 'circle_image.dart';
 
@@ -7,15 +8,18 @@ class ProductCard extends StatelessWidget {
   const ProductCard({required this.product, super.key});
 
   void selectProduct(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed("/product", arguments: product);
+    Navigator.of(ctx).pushNamed("/product", arguments: product.id);
   }
 
   @override
   Widget build(BuildContext context) {
+    String category = product.product_categories[0]["categories"]["category"];
     return InkWell(
       onTap: () => selectProduct(context),
       child: Card(
-        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        margin: EdgeInsets.symmetric(vertical: 4),
+        elevation: 2.5,
         child: Container(
           padding: const EdgeInsets.fromLTRB(20, 10, 40, 10),
           child: Row(
@@ -24,17 +28,22 @@ class ProductCard extends StatelessWidget {
               Row(
                 children: [
                   Container(
-                    child: const CircleImage(
-                        url:
-                            'http://192.168.0.103:3000/static/images/store/default.jpg'),
+                    child: CircleImage(
+                      url: '${Config.staticUrl}${product.images[0]["image"]}',
+                      radius: 30,
+                    ),
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
                           padding: const EdgeInsets.only(bottom: 10),
-                          child: Text(product.name)),
-                      Text("category")
+                          child: Text(
+                            product.name,
+                            style: Theme.of(context).textTheme.headline3,
+                          )),
+                      Text(
+                          "${category[0].toUpperCase()}${category.substring(1)} ")
                     ],
                   ),
                 ],
@@ -44,8 +53,8 @@ class ProductCard extends StatelessWidget {
                 children: [
                   Container(
                       padding: const EdgeInsets.only(bottom: 10),
-                      child: Text("discount")),
-                  Text("distance")
+                      child: Text("up to ${product.discount}%")),
+                  Text("1Km")
                 ],
               )
             ],
