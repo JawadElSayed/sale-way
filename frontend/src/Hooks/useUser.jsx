d46eaa7e660e5439576b5d1149fc33d565e262bb
JawadElSayed <jawadelsayed3@gmail.com>
2022-11-25 14:05:38 +0200

    frontend > creating add user

--- frontend/src/Hooks/useUser.jsx
+++ frontend/src/Hooks/useUser.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { sale_way } from "../utils/axios";
 
 export const ALL_USER_KEY = ["ALL_USER_KEY"];
@@ -7,6 +7,14 @@ const getAllUsers = async () => {
 	return await sale_way.getAPI(`/user`);
 };
 
+const AddUser = async (data) => {
+	return await sale_way.postAPI("/user", data);
+};
+
 export const useAllUsers = () => {
 	return useQuery(ALL_USER_KEY, getAllUsers);
+};
+
+export const useAddUser = () => {
+	return useMutation(AddUser);
 };
